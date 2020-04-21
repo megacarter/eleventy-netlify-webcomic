0b41159173dd8ba33f51999b5ab8e332a10d3755
Kate Drwecka <katedrwecka@gmail.com>
2020-04-20 20:37:57 -0400

    only watch sass for local dev

--- config/sass-process.js
+++ config/sass-process.js
@@ -14,12 +14,15 @@ module.exports = (scssPath, cssPath) => {
         .catch(error => console.error(error))
     }
     //Watch for changes to scssPath directory...
-    fs.watch(path.dirname(scssPath), () => {
-        //Render css from sass...
-        sass.render({file: scssPath})
-        //Then write result css string to cssPath file
-        .then(result => fs.writeFile(cssPath, result.css.toString()))
-        .catch(error => console.error(error))
-        console.log(`Watching ${path.dirname(scssPath)}...`);      
-    });
+    //Only if running local dev
+    if (process.env.NODE_ENV === 'localdev') {
+        fs.watch(path.dirname(scssPath), () => {
+            //Render css from sass...
+            sass.render({file: scssPath})
+            //Then write result css string to cssPath file
+            .then(result => fs.writeFile(cssPath, result.css.toString()))
+            .catch(error => console.error(error))
+            console.log(`Watching ${path.dirname(scssPath)}...`);      
+        });
+    }
 }
